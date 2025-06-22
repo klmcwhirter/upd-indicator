21e5b952d25a0b803cf65e5940a7220311cf7fb2
Kevin McWhirter <klmcw@yahoo.com>
2025-06-22 10:01:53 -0700

    Add -v option to rpm-ostree-update-check.sh; remove spaces from rule names; remove debug echo statement from upd_monitor.sh.in

--- etc/scripts/rpm-ostree-update-check.sh
+++ etc/scripts/rpm-ostree-update-check.sh
@@ -1,5 +1,10 @@
 
-rpm-ostree upgrade --check --unchanged-exit-77 >/dev/null 2>&1
+if [ "$1" = "-v" ]
+then
+  rpm-ostree upgrade --check --unchanged-exit-77 1>&2
+else
+  rpm-ostree upgrade --check --unchanged-exit-77 >/dev/null 2>&1
+fi
 rc=$?
 
 if [[ $rc == 77 ]]
